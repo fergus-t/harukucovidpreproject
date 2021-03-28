8f3edb9aa2d4d51bcf7380cb82c54e9c16cf56be
fergus-t <62132478+fergus-t@users.noreply.github.com>
2021-03-29 01:04:11 +0800

    Delete orders directory

--- orders/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class locationForm(forms.Form):
-    countryName = forms.CharField(label='Location Name', max_length=50)
-    apisource = forms.CharField(label='API Source', max_length=200)
-    resourceurl = forms.CharField(label='Resourceurl', max_length=200)
-    population = forms.IntegerField(label='Population')
--- orders/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Location(models.Model): 
-    country = models.CharField(max_length=50)
-    apisource = models.CharField(max_length=200)
-    resourceurl = models.CharField(max_length=200)
-    population = models.BigIntegerField()
